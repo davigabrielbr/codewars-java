1f5c3827eb1b17c2056a4ef93810b0e00f874671
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-09-05 14:07:18 -0300

    Add Codewars ReturningStrings exercise

---
+++ src/kyu8/ReturningStrings.java
@@ -0,0 +1,16 @@
+package kyu8;
+
+/**Create a function that accepts a parameter representing a name
+ * and returns the message: "Hello, <name> how are you doing today?".
+
+ * [Make sure you type the exact thing I wrote or the program may not execute properly]*/
+
+public class ReturningStrings {
+    public static void main(String[] args) {
+        System.out.println(greet("Ryan"));
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
