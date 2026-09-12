bf1138cf25f1b4a81d125f170a1f77f71502ad3a
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-09-12 18:47:05 -0300

    Add Codewars Java exercise

---
+++ src/kyu8/Java.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+// Code as fast as you can! You need to double the integer and return it.
+
+public class Java {
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(5));
+    }
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
