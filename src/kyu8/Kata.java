d2622ca2710fa5ed278c48337bcf206a666da5a7
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-09-12 19:01:18 -0300

    Add Codewars Kata exercise

---
+++ src/kyu8/Kata.java
@@ -0,0 +1,30 @@
+package kyu8;
+
+/*Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+        "found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+Note: In COBOL, it should return "found the needle at position 6"*/
+
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println(findNeedle(new Object[]{"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"}));
+    }
+
+    public static String findNeedle(Object[] haystack) {
+        for (int i = 0; i < haystack.length; i++) {
+            if ("needle".equals(haystack[i])) {
+                return "found the needle at position " + i;
+            }
+        }
+
+        return "found the needle at position -1";
+    }
+}
